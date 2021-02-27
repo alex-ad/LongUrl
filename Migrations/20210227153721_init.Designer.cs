e2035ff86a9238e70bdf50dc83180b9dd29c2994
Alexandr Grigoriev <grigoriev.a.v.1980@gmail.com>
2021-02-27 18:37:43 +0300

    fix db

--- Migrations/20210227153721_init.Designer.cs
+++ Migrations/20210227153721_init.Designer.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LongUrl.Migrations
 {
     [DbContext(typeof(TokenDbContext))]
-    [Migration("20210227151516_init")]
+    [Migration("20210227153721_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
